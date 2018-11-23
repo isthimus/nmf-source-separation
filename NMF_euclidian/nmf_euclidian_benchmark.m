a17a5875154c9fa4b98f8e76f61d0d88521babf0
Creighton MK Mr (UG - Elec Electronic Eng) <mc00385@surrey.ac.uk>
2018-11-23 16:18:05 +0000

    renamed datasets to be more logical. started learning about spectrograms and stfts. removed erroneous __MACOSX folder in datasetsx.

--- NMF_euclidian/nmf_euclidian_benchmark.m
+++ NMF_euclidian/nmf_euclidian_benchmark.m
@@ -1,1 +1,26 @@
-% TODO: cite TRIOS dataset
+PROJECT_PATH = '/user/HS124/mc00385/third_year_project/score-aware-source-separation';
+TRIOS_PATH = fullfile(PROJECT_PATH, '/datasets/TRIOS');
+
+[vln_brahms, Fs] = audioread(fullfile(TRIOS_PATH, 'brahms/violin.wav'));
+pno_syn_brahms = audioread(fullfile(TRIOS_PATH, 'brahms/piano_syn.wav'));
+
+n = [1 : length(vln_brahms)];
+
+plot (n,vln_brahms);
+waitforbuttonpress
+
+colormap bone
+spectrogram(vln_brahms, ceil(Fs/50), [],[], Fs, 'MinThreshold',-100,  'yaxis');
+xlabel('time (samples)')
+
+waitforbuttonpress
+
+%spectrogram(vln_brahms, ceil(Fs/50), [],[], Fs, 'MinThreshold',-110,  'yaxis');
+spectrogram(vln_brahms, ceil(Fs/50), 'yaxis');
+view(-30,65)
+xlabel('time (samples)')
+colorbar off 
+
+waitforbuttonpress
+close all
+
