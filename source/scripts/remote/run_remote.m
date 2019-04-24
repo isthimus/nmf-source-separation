4581d2408179238ea7f406b9a86d1272fc07847d
Matt Creighton <mc00385@surrey.ac.uk>
2019-04-24 12:38:57 +0100

    big folder restructuring, plus several updates to next.txt. changes: scripts/bench -> scripts/bench_midterm, new bench folder in /source/. will have plenty of subfolders for the different tests. restructured /source/align/align ready for onset and other such lovelies. removed scripts/remote. terrible idea and i have a new laptop now. placeholders for source/nmf/spect and source/user

--- source/scripts/remote/run_remote.m
+++
@@ -1,8 +0,0 @@
-% Turn on recording of the command window output
-diary('tmp_matlab_diary.txt');
-disp('Hello, World!');
-fileToRun = getenv('MKC_MATLAB_FILETORUN');
-run(fileToRun);
-% Turn recording back off
-diary off;
-exit;
--- source/scripts/remote/run_remote.sh
+++
@@ -1,3 +0,0 @@
-MKC_MATLAB_FILETORUN=$1
-echo ${MKC_MATLAB_FILETORUN}
-matlab -r ${MKC_MATLAB_FILETORUN}\;exit\; -nosplash -nodesktop -wait -log
